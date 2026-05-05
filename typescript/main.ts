b363110f45f8dcf02d0d5ed06d739866bee4a3dc
DevKoko <devkoko.vt@gmail.com>
2026-05-05 00:26:55 -0400

    typescript - floor 2

--- typescript/main.ts
+++ typescript/main.ts
@@ -2,10 +2,27 @@ import net from "node:net";
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log("Client connected with data: " + data);
+    const req = data.toString();
+    console.log(req);
 
-    // Send 200 OK when client connects, ignore data for now
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const lines = req.split("\n");
+    const reqLine = lines[0];
+    const details = reqLine?.split(" ");
+
+    const path = details![1];
+    
+    if(path === "/") {
+      // Send 200 OK when client connects, ignore data for now
+      const ok = "HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(ok);
+      console.log(ok);
+    }
+    else {
+      // Send 404 Not Found for every other path
+      const notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
+      socket.write(notFound);
+      console.log(notFound);
+    }
   });
 
   socket.on("end", () => {
