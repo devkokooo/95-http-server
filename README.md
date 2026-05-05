7c8caf42f1d4f7e3aa08b0bed00d3d3cc2c85e8d
DevKoko <devkoko.vt@gmail.com>
2026-05-05 00:15:45 -0400

    typescript - floor 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build Your Own **HTTP Server**
+
+https://www.95ninefive.dev/challenges/build-your-own-http-server
--- typescript/main.ts
+++ typescript/main.ts
@@ -3,7 +3,9 @@ import net from "node:net";
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     console.log("Client connected with data: " + data);
-    socket.write("Echo: " + data);
+
+    // Send 200 OK when client connects, ignore data for now
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
   });
 
   socket.on("end", () => {
